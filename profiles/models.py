7bd1e0855a646fd2805c7fbd86af31dc9727d3d2
Anis Lazhar <anis.lazhar@web.de>
2024-07-15 17:06:58 +0000

    started project, added profiles app

---
+++ profiles/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    creator = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255 , blank=True)
+    country = models.CharField(max_length=255 , blank=True)
+    description = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    is_featured = models.BooleanField(default=False)
+    image = models.ImageField(
+        upload_to='images/',
+        default='../dinner.219fa9d437e9'
+    )
+
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.creator}'s profile"
+
+# Function for creating a profile, when a new user is created
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(creator=instance)
+
+# This is a signal to watch for a new user registering 
+# It will then create a profile 
+post_save.connect(create_profile, sender=User)
